f433c6f3704696d3baa5759756d4d0205900fb32
Bohdan <phase1912@gmail.com>
2025-07-13 16:58:12 +0200

    fix

--- src/components/task2/friendListItem.jsx
+++ src/components/task2/friendListItem.jsx
@@ -1,4 +1,4 @@
-import styles from './FriendListItem.module.css';
+import styles from './friendListItem.module.css';
 import clsx from "clsx";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
